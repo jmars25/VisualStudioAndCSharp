cc78cf63fb2b4771c685efc67a670ec17a924b78
jmars25 <jmarsico25@gmail.com>
2021-02-02 08:14:21 -0500

    Create StringReverse.cs

---
+++ StringReverse.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace StringReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ask for string and displays
+            String palindrome, revString = "";
+            Console.WriteLine("check to see if your string is a palindrome!");
+            palindrome = Console.ReadLine();
+            Console.WriteLine("your string is {0}",palindrome);
+
+            //reverses the string
+            for (int i=palindrome.Length-1; i>=0; i--)
+            {
+                revString = revString + palindrome[i];
+            }
+
+            //decides if its a palindrome or not and displays result
+            Console.WriteLine("your string reversed is {0}",revString);
+            if (palindrome == revString)
+            {
+                Console.WriteLine("it is a palindrome!");
+
+            }
+
+            else
+                Console.WriteLine("it is not a palindrome ):");
+        }
+
+     
+}
+
+
+
+
+
+    }
+
